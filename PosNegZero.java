4cb56cb1c72628016b8c66ca5592cd50fac8e512
Pradeepnataraj <42169771+Pradeepnataraj@users.noreply.github.com>
2018-08-07 14:43:43 +0530

    Update PosNegZero.java

--- PosNegZero.java
+++ PosNegZero.java
@@ -1,14 +1,21 @@
+import java.util.*;
+ 
 class PosNegZero
 {
-    public static void main(String args[])
+    public static void main(String []s)
     {
         int a;
+        
+        Scanner a=new Scanner(System.in);
+         
+        System.out.print("Enter any integer number: ");
+       
         if(a>0)
-            System.out.println("POSITIVE NUMBER");
-        else if(num<0)
-            System.out.println(" NEGATIVE NUMBER");
-        else(a==0)
-            System.out.println(" ZERO");
+            System.out.println(num + " is POSITIVE NUMBER.");
+        else if(a<0)
+            System.out.println(num + " is NEGATIVE NUMBER.");
+        else
+            System.out.println("IT's ZERO.");
          
     }
 }
