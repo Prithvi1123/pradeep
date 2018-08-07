7dfa03cabbc29291f92757cca194ee2c16297b03
Pradeepnataraj <42169771+Pradeepnataraj@users.noreply.github.com>
2018-08-07 14:33:39 +0530

    Create PosNegZero.java

---
+++ PosNegZero.java
@@ -0,0 +1,14 @@
+class PosNegZero
+{
+    public static void main(String args[])
+    {
+        int a;
+        if(a>0)
+            System.out.println("POSITIVE NUMBER");
+        else if(num<0)
+            System.out.println(" NEGATIVE NUMBER");
+        else(a==0)
+            System.out.println(" ZERO");
+         
+    }
+}
