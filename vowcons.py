f66e12d232f12f365af98d3a7e3c8f4ebbf58a76
Pradeepnataraj <42169771+Pradeepnataraj@users.noreply.github.com>
2019-06-07 11:07:04 +0530

    Update and rename vowcons.java to vowcons.py

---
+++ vowcons.py
@@ -0,0 +1,7 @@
+m=input()
+m=ord(m)
+if ((m>=65&m<=90)or(m=97&m=122)):
+    if m=65 or m=69 or m=73 or m=79 or m=85 or m=97 or m=101 or m=105 or m=111 or m=117:
+        print('Vowel')
+    else:print('Consonant') 
+else:print('Invalid')
